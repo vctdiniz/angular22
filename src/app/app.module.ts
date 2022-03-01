1fd0b94d8b4045cfd173fc858833ba212ae8acdd
Victor <vctdiniz@gmail.com>
2022-02-28 22:57:57 -0300

    add interceptor and loading service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
@@ -9,12 +9,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DatagridComponent } from './components/datagrid/datagrid.component';
 import { DefaultComponent } from './components/default/default.component';
+import { AsyncTestComponent } from './components/async-test/async-test.component';
+import { NetworkInterceptor } from './network.interceptor';
 
 @NgModule({
   declarations: [
     AppComponent,
     DatagridComponent,
-    DefaultComponent
+    DefaultComponent,
+    AsyncTestComponent
   ],
   imports: [
     BrowserModule,
@@ -26,7 +29,9 @@ import { DefaultComponent } from './components/default/default.component';
     }),
     FormsModule
   ],
-  providers: [],
+  providers: [{
+    provide: HTTP_INTERCEPTORS, useClass: NetworkInterceptor, multi: true
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
