2b379035f7d7ef1134750c992e617667705f101c
Victor <vctdiniz@gmail.com>
2022-03-02 23:18:44 -0300

    add custom-modal

--- src/app/components/async-test/async-test.component.ts
+++ src/app/components/async-test/async-test.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { BsModalService } from 'ngx-bootstrap/modal';
 import { Observable } from 'rxjs';
+import { User } from 'src/app/app.model';
 import { LoadingService } from 'src/app/loading.service';
 
 import { AsyncTestService } from './async-test.service';
@@ -14,15 +16,26 @@ export class AsyncTestComponent implements OnInit {
   items$!: Observable<any[]>;
   loading$ = this.loader.loading$;
 
-  constructor(private service: AsyncTestService, public loader: LoadingService) {
-    this.items$ = this.service.getItems();
-  }
+  constructor(
+    private service: AsyncTestService,
+    public loader: LoadingService) { }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    this.items$ = this.service.getItems();
+   }
 
   onClickAdd(form: NgForm) {
     this.service.addItem(form.value);
     form.reset();
+  }
+
+  onClickEdit(user: User) {
+    console.log('onClickEdit >> ', user);
+    this.service.updateItem(user);
+  }
+
+  onClickDelete(user: User) {
+    console.log('onClickDelete >> ', user);
   }
 
   // test1() {
